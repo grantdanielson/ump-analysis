75775f74a4f37392aa7150521e436ff9d0f5979c
grantdanielson <grantdanielson@aol.com>
2024-04-10 04:11:16 -0400

    Added plots and documentation

--- ump-analysis/call_analysis.py
+++ ump-analysis/call_analysis.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import os
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 import pybaseball as pyball
 import retrosheet_exec
 from df_headers import df_headers
@@ -34,18 +36,16 @@ def _correct_call(df_sc: pd.DataFrame) -> int:
     sz_top: float = df_sc['sz_top']
     sz_bot: float = df_sc['sz_bot']
     sz_hor: float = 0.83
-    baseball_rad: float = 0.120833
+    baseball_rad: float = 0.1208333
     correct_call: int = 1
 
-    if (abs(plate_x) > sz_hor) and (call_type == 'S'):
+    if (abs(plate_x) > sz_hor) and (call_type == 'S'): # Outside of strikezone horizontally, called strike
         correct_call = 0
-    elif (abs(plate_x) < sz_hor) and (plate_x < (sz_top + baseball_rad)) and (plate_x > (sz_bot - baseball_rad)) and (call_type == 'B'):
+    elif (abs(plate_x) < sz_hor) and (plate_z < (sz_top + baseball_rad)) and (plate_z > (sz_bot - baseball_rad)) and (call_type == 'B'): # In strikezone, called ball
         correct_call = 0
-    elif (plate_z < (sz_top + baseball_rad)) and (plate_z > (sz_bot - baseball_rad)) and (abs(plate_x) < sz_hor) and (call_type == 'B'):
+    elif (plate_z > (sz_top + baseball_rad)) and (call_type == 'S'): # High, called strike
         correct_call = 0
-    elif (plate_z > (sz_top + baseball_rad)) and (call_type == 'S'):
-        correct_call = 0
-    elif (plate_z < (sz_bot - baseball_rad)) and (call_type == 'S'):
+    elif (plate_z < (sz_bot - baseball_rad)) and (call_type == 'S'): # Low, called strike
         correct_call = 0
     return correct_call
 
@@ -59,35 +59,38 @@ def red_analysis(df_sc: pd.DataFrame) -> pd.DataFrame:
     df_re288.index = df_headers('re288-index') 
     df_re288.columns = df_headers('re288-columns')
     # Create new column called 'run_expectancy_delta' with _red_analyis logic
-    df_sc['run_expectancy_delta'] = df_sc.apply(_red_analyis(df_sc, df_re288), axis = 1)
+    df_sc['missed_call_red'] = df_sc.apply(_red_analysis, axis = 1, args = [df_re288])
     return df_sc
 
-def _red_analyis(df_sc: pd.DataFrame) -> str:
-    df_re288 = pd.DataFrame(data = pd.read_csv('./ump-analysis/re288.csv', header = None))
-    df_re288.index = df_headers('re288-index') 
-    df_re288.columns = df_headers('re288-columns')    
-    print(df_re288)
-    first_base: str = '_'
-    second_base: str = '_'
-    third_base: str = '_'
 
-    if pd.notnull(df_sc['on_1b']):
-        first_base = '1'
-    if pd.notnull(df_sc['on_2b']):
-        second_base = '2'
-    if pd.notnull(df_sc['on_3b']):
-        third_base = '3'
-    
-    out_bases: str = f'{df_sc['outs_when_up']} {first_base}{second_base}{third_base}'
-    count: str = f'[{df_sc['balls']},{df_sc['strikes']}]'
-    print(f'ob: {out_bases}, count: {count}')
-    red = df_re288.loc[out_bases, count]
-    return red
+def _red_analysis(df_sc: pd.DataFrame, df_re288: pd.DataFrame) -> float:
+    """
+    Finds outs, if batters are on bases, and the count, then matches
+    the situation to the RED matrix if there is a missed call.
+    """
+    if df_sc['correct_call'] == 0:
+        first_base: str = '_'
+        second_base: str = '_'
+        third_base: str = '_'
+
+        if pd.notnull(df_sc['on_1b']):
+            first_base = '1'
+        if pd.notnull(df_sc['on_2b']):
+            second_base = '2'
+        if pd.notnull(df_sc['on_3b']):
+            third_base = '3'
+        
+        out_bases: str = f'{df_sc['outs_when_up']} {first_base}{second_base}{third_base}'
+        count: str = f'[{df_sc['balls']}-{df_sc['strikes']}]'
+        red = df_re288.loc[out_bases, count]
+        return red
 
 
 def import_umpires(season, type = 'regular', exec_type = 'game', export_dir = './ump-analysis/retrosheet'):
     """
     Import umpire data from retrosheets and match it to statcast.
+
+    Will probably delete since I don't think I need it (rip like 6 hours)
     """
     if not os.path.isfile(f'{export_dir}/{season}events.csv'):
         retrosheet_exec.pull_event_files(season, type, export_dir)
@@ -100,26 +103,100 @@ def import_umpires(season, type = 'regular', exec_type = 'game', export_dir = '.
     return df_rs
 
 
-def remove_empty():
+def missed_call_plotting(df_sc: pd.DataFrame):
     """
-    Removes columns that have no data at all.
+    Adds partial working plotting with strike zone layer.
     """
-    #df_sc.dropna(how = 'all', axis = 1, inplace = True)
+    sz_top_avg: float = df_sc.loc[:, 'sz_top'].mean()
+    sz_bot_avg: float = df_sc.loc[:, 'sz_bot'].mean()
+    sz_hor: float = 0.83
+    df_mc = df_sc[['type', 'plate_x', 'plate_z', 'sz_top', 'sz_bot', 'correct_call']]
+    df_mc = df_mc[df_mc['correct_call'].isin([0])]
 
+    df_mc['plate_x_S'] = df_mc.apply(x_from_sz, axis = 1, args = ['S'])
+    df_mc['plate_z_S'] = df_mc.apply(z_from_sz, axis = 1, args = ['S', sz_top_avg, sz_bot_avg])
+    df_mc['plate_x_B'] = df_mc.apply(x_from_sz, axis = 1, args = ['B'])
+    df_mc['plate_z_B'] = df_mc.apply(z_from_sz, axis = 1, args = ['B', sz_top_avg, sz_bot_avg])
 
+    plate_x_S_array: np.array = df_mc['plate_x_S']
+    plate_x_S_array = plate_x_S_array[~np.isnan(plate_x_S_array)]
+    plate_z_S_array: np.array = df_mc['plate_z_S']
+    plate_z_S_array = plate_z_S_array[~np.isnan(plate_z_S_array)]
+    plate_x_B_array: np.array = df_mc['plate_x_B']
+    plate_x_B_array = plate_x_B_array[~np.isnan(plate_x_B_array)]
+    plate_z_B_array: np.array = df_mc['plate_z_B']
+    plate_z_B_array = plate_z_B_array[~np.isnan(plate_z_B_array)]
 
-#remove_empty()
+    fig, ax = plt.subplots()
+    ax.scatter(plate_x_S_array, plate_z_S_array)
+    ax.scatter(plate_x_B_array, plate_z_B_array)
+    ax.add_patch(Rectangle((-sz_hor, sz_bot_avg),
+                           (sz_hor * 2), (sz_top_avg - sz_bot_avg),
+                           edgecolor = 'black',
+                           facecolor = 'none'))
+    plt.show()
+    return
+
+def x_from_sz(df_mc: pd.DataFrame, call_type: str) -> float:
+    """
+    Logic for whether or not ot return a value.
+    """
+    x_pos: float = None
+    match call_type:
+        case 'S': # Determined when calling this function
+            if df_mc['type'] == 'S': # Checking to see this rows 'type' matches the call_type argument
+                x_pos = df_mc['plate_x']
+        case 'B': # Same as above, but with B instead of S
+            if df_mc['type'] == 'B': 
+                x_pos = df_mc['plate_x']
+    return x_pos
+
+def z_from_sz(df_mc: pd.DataFrame, call_type: str, sz_top_avg: float, sz_bot_avg: float) -> float:
+    """
+    Logic for determining the position for the y-axis of the scatter plot.
+    
+    Works but needs to optomized/cleaned
+    """
+    z_pos = None
+    match call_type:
+        case 'S': # Determined when calling this function
+            if df_mc['type'] == 'S': # Checking to see this rows 'type' matches the call_type argument
+                if df_mc['plate_z'] > df_mc['sz_top']: # If ball is above sz
+                    relative_pos = df_mc['plate_z'] - df_mc['sz_top']
+                    z_pos = relative_pos + sz_top_avg
+                elif df_mc['plate_z'] < df_mc['sz_bot']: # If ball is below sz
+                    relative_pos = df_mc['sz_bot'] - df_mc['plate_z']
+                    z_pos = sz_bot_avg - relative_pos # If ball is in uppper half of sz
+                elif (df_mc['sz_top'] - df_mc['plate_z']) > (df_mc['plate_z'] - df_mc['sz_bot']):
+                    relative_pos = df_mc['sz_top'] - df_mc['plate_z']
+                    z_pos = sz_top_avg - relative_pos
+                else: # If ball is in sz, but not he upper half
+                    relative_pos = df_mc['plate_z'] - df_mc['sz_bot']
+                    z_pos = relative_pos + sz_bot_avg
+        case 'B': # Same as above, but with B instead of S
+            if df_mc['type'] == 'B':
+                if df_mc['plate_z'] > df_mc['sz_top']:
+                    relative_pos = df_mc['plate_z'] - df_mc['sz_top']
+                    z_pos = relative_pos + sz_top_avg
+                elif df_mc['plate_z'] < df_mc['sz_bot']:
+                    relative_pos = df_mc['sz_bot'] - df_mc['plate_z']
+                    z_pos = sz_bot_avg - relative_pos
+                elif (df_mc['sz_top'] - df_mc['plate_z']) > (df_mc['plate_z'] - df_mc['sz_bot']):
+                    relative_pos = df_mc['sz_top'] - df_mc['plate_z']
+                    z_pos = sz_top_avg - relative_pos
+                else:
+                    relative_pos = df_mc['plate_z'] - df_mc['sz_bot']
+                    z_pos = relative_pos + sz_bot_avg
+    return z_pos
 
 #df_rs = pd.DataFrame(data=pyball.retrosheet.season_game_logs('2023'))
 
 df_sc = correct_call(start_dt = "2023-03-30", end_dt = "2023-04-01")
 
-df_sc['run_expectancy_delta'] = df_sc.apply(_red_analyis, axis = 1)
-#df_rs = import_umpires('2023')
-#df_pm = pd.DataFrame(data = pd.read_csv('player-map.csv'))
-#df_pm.columns = [['IDPLAYER', 'PLAYERNAME', 'BIRTHDATE', 'FIRSTNAME', 'LASTNAME', 'TEAM', 'LG', 'POS', 'IDFANGRAPHS', 'FANGRAPHSNAME', 'MLBID', 'MLBNAME', 'CBSID', 'CBSNAME', 'RETROID', 'BREFID', 'NFBCID', 'NFBCNAME', 'ESPNID', 'ESPNNAME', 'KFFLNAME', 'DAVENPORTID', 'BPID', 'YAHOOID', 'YAHOONAME', 'MSTRBLLNAME', 'BATS', 'THROWS', 'FANTPROSNAME', 'LASTCOMMAFIRST', 'ROTOWIREID', 'FANDUELNAME', 'FANDUELID', 'DRAFTKINGSNAME', 'OTTONEUID', 'HQID', 'RAZZBALLNAME', 'FANTRAXID', 'FANTRAXNAME', 'ROTOWIRENAME', 'ALLPOS', 'NFBCLASTFIRST', 'ACTIVE', 'UNDERDOG']]
-#print(df_pm)
-#df_pm_use = df_pm[['FIRSTNAME','LASTNAME','POS','MLBID','RETROID']]
-#df_pm_use.to_csv('./players-maped.csv')
+#missed_call_plotting(df_sc)
 
-#df_sc.to_csv("statcast_data_calls_all.csv", index=False, encoding='utf8')
+#df_sc = red_analysis(df_sc)
+
+#print(df_sc)
+
+#df_sc.to_csv("statcast_data_calls_all_red.csv", index=False, encoding='utf8')
