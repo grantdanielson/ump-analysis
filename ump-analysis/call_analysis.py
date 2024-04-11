355b3fd96cb6de836703970a9ddd86c7b5fd96e6
grantdanielson <grantdanielson@aol.com>
2024-04-11 03:46:27 -0400

    Added heatmap (kinda) plotting and fixed some variables/documentation

--- ump-analysis/call_analysis.py
+++ ump-analysis/call_analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import numpy as np
 import os
 import matplotlib.pyplot as plt
+import seaborn as sns
 from matplotlib.patches import Rectangle
 import pybaseball as pyball
 import retrosheet_exec
@@ -35,17 +35,17 @@ def _correct_call(df_sc: pd.DataFrame) -> int:
     plate_z: float = df_sc['plate_z']
     sz_top: float = df_sc['sz_top']
     sz_bot: float = df_sc['sz_bot']
-    sz_hor: float = 0.83
-    baseball_rad: float = 0.1208333
+    sz_hor: float = (17/12) / 2 
+    ball_rad: float = (2.94/12) / 2
     correct_call: int = 1
 
     if (abs(plate_x) > sz_hor) and (call_type == 'S'): # Outside of strikezone horizontally, called strike
         correct_call = 0
-    elif (abs(plate_x) < sz_hor) and (plate_z < (sz_top + baseball_rad)) and (plate_z > (sz_bot - baseball_rad)) and (call_type == 'B'): # In strikezone, called ball
+    elif (abs(plate_x) < sz_hor) and (plate_z < (sz_top + ball_rad)) and (plate_z > (sz_bot - ball_rad)) and (call_type == 'B'): # In strikezone, called ball
         correct_call = 0
-    elif (plate_z > (sz_top + baseball_rad)) and (call_type == 'S'): # High, called strike
+    elif (plate_z > (sz_top + ball_rad)) and (call_type == 'S'): # High, called strike
         correct_call = 0
-    elif (plate_z < (sz_bot - baseball_rad)) and (call_type == 'S'): # Low, called strike
+    elif (plate_z < (sz_bot - ball_rad)) and (call_type == 'S'): # Low, called strike
         correct_call = 0
     return correct_call
 
@@ -105,98 +105,54 @@ def import_umpires(season, type = 'regular', exec_type = 'game', export_dir = '.
 
 def missed_call_plotting(df_sc: pd.DataFrame):
     """
-    Adds partial working plotting with strike zone layer.
+    Adds partially working plotting with strike zone layer.
     """
     sz_top_avg: float = df_sc.loc[:, 'sz_top'].mean()
     sz_bot_avg: float = df_sc.loc[:, 'sz_bot'].mean()
-    sz_hor: float = 0.83
+    sz_hor: float = (17/12) / 2 
+    ball_rad: float = 0.1208333
+
     df_mc = df_sc[['type', 'plate_x', 'plate_z', 'sz_top', 'sz_bot', 'correct_call']]
     df_mc = df_mc[df_mc['correct_call'].isin([0])]
 
-    df_mc['plate_x_S'] = df_mc.apply(x_from_sz, axis = 1, args = ['S'])
-    df_mc['plate_z_S'] = df_mc.apply(z_from_sz, axis = 1, args = ['S', sz_top_avg, sz_bot_avg])
-    df_mc['plate_x_B'] = df_mc.apply(x_from_sz, axis = 1, args = ['B'])
-    df_mc['plate_z_B'] = df_mc.apply(z_from_sz, axis = 1, args = ['B', sz_top_avg, sz_bot_avg])
+    df_mc['plate_z_rel'] = df_mc.apply(z_from_sz, axis = 1, args = [sz_top_avg, sz_bot_avg])
 
-    plate_x_S_array: np.array = df_mc['plate_x_S']
-    plate_x_S_array = plate_x_S_array[~np.isnan(plate_x_S_array)]
-    plate_z_S_array: np.array = df_mc['plate_z_S']
-    plate_z_S_array = plate_z_S_array[~np.isnan(plate_z_S_array)]
-    plate_x_B_array: np.array = df_mc['plate_x_B']
-    plate_x_B_array = plate_x_B_array[~np.isnan(plate_x_B_array)]
-    plate_z_B_array: np.array = df_mc['plate_z_B']
-    plate_z_B_array = plate_z_B_array[~np.isnan(plate_z_B_array)]
-
-    fig, ax = plt.subplots()
-    ax.scatter(plate_x_S_array, plate_z_S_array)
-    ax.scatter(plate_x_B_array, plate_z_B_array)
-    ax.add_patch(Rectangle((-sz_hor, sz_bot_avg),
-                           (sz_hor * 2), (sz_top_avg - sz_bot_avg),
+    fig, ax = plt.subplots(figsize=(4, 5))
+    sns.scatterplot(df_mc, x = 'plate_x', y = 'plate_z_rel')
+    #sns.kdeplot(df_mc, x = 'plate_x', y = 'plate_z_rel',
+                #cmap = 'flare', cut = 1,
+    ax.add_patch(Rectangle((-sz_hor, sz_bot_avg ),
+                           (sz_hor * 2), ((sz_top_avg - sz_bot_avg)),
                            edgecolor = 'black',
                            facecolor = 'none'))
+    ax.axis('off')
     plt.show()
     return
 
-def x_from_sz(df_mc: pd.DataFrame, call_type: str) -> float:
+def z_from_sz(df_mc: pd.DataFrame, sz_top_avg: float, sz_bot_avg: float, ball_rad: float = 0.1208333) -> float:
     """
-    Logic for whether or not ot return a value.
-    """
-    x_pos: float = None
-    match call_type:
-        case 'S': # Determined when calling this function
-            if df_mc['type'] == 'S': # Checking to see this rows 'type' matches the call_type argument
-                x_pos = df_mc['plate_x']
-        case 'B': # Same as above, but with B instead of S
-            if df_mc['type'] == 'B': 
-                x_pos = df_mc['plate_x']
-    return x_pos
-
-def z_from_sz(df_mc: pd.DataFrame, call_type: str, sz_top_avg: float, sz_bot_avg: float) -> float:
-    """
-    Logic for determining the position for the y-axis of the scatter plot.
-    
-    Works but needs to optomized/cleaned
+    Logic for determining the position for the y-axis of the plot.
+    Returns a value relative to the average strike-zone.
     """
     z_pos = None
-    match call_type:
-        case 'S': # Determined when calling this function
-            if df_mc['type'] == 'S': # Checking to see this rows 'type' matches the call_type argument
-                if df_mc['plate_z'] > df_mc['sz_top']: # If ball is above sz
-                    relative_pos = df_mc['plate_z'] - df_mc['sz_top']
-                    z_pos = relative_pos + sz_top_avg
-                elif df_mc['plate_z'] < df_mc['sz_bot']: # If ball is below sz
-                    relative_pos = df_mc['sz_bot'] - df_mc['plate_z']
-                    z_pos = sz_bot_avg - relative_pos # If ball is in uppper half of sz
-                elif (df_mc['sz_top'] - df_mc['plate_z']) > (df_mc['plate_z'] - df_mc['sz_bot']):
-                    relative_pos = df_mc['sz_top'] - df_mc['plate_z']
-                    z_pos = sz_top_avg - relative_pos
-                else: # If ball is in sz, but not he upper half
-                    relative_pos = df_mc['plate_z'] - df_mc['sz_bot']
-                    z_pos = relative_pos + sz_bot_avg
-        case 'B': # Same as above, but with B instead of S
-            if df_mc['type'] == 'B':
-                if df_mc['plate_z'] > df_mc['sz_top']:
-                    relative_pos = df_mc['plate_z'] - df_mc['sz_top']
-                    z_pos = relative_pos + sz_top_avg
-                elif df_mc['plate_z'] < df_mc['sz_bot']:
-                    relative_pos = df_mc['sz_bot'] - df_mc['plate_z']
-                    z_pos = sz_bot_avg - relative_pos
-                elif (df_mc['sz_top'] - df_mc['plate_z']) > (df_mc['plate_z'] - df_mc['sz_bot']):
-                    relative_pos = df_mc['sz_top'] - df_mc['plate_z']
-                    z_pos = sz_top_avg - relative_pos
-                else:
-                    relative_pos = df_mc['plate_z'] - df_mc['sz_bot']
-                    z_pos = relative_pos + sz_bot_avg
+    if df_mc['plate_z'] > (df_mc['sz_top'] + ball_rad) : # If ball is above sz
+        relative_pos = df_mc['plate_z'] - df_mc['sz_top']
+        z_pos = relative_pos + sz_top_avg
+    elif df_mc['plate_z'] < (df_mc['sz_bot'] - ball_rad): # If ball is below s
+        relative_pos = df_mc['sz_bot'] - df_mc['plate_z']
+        z_pos = sz_bot_avg - relative_pos
+    elif ((df_mc['sz_top'] + ball_rad) - df_mc['plate_z']) > (df_mc['plate_z'] - (df_mc['sz_bot'] - ball_rad)): # If ball is in uppper half of sz
+        relative_pos = df_mc['sz_top'] - df_mc['plate_z']
+        z_pos = sz_top_avg - relative_pos
+    else: # If ball is in sz, but not he upper half
+        relative_pos = df_mc['plate_z'] - df_mc['sz_bot']
+        z_pos = relative_pos + sz_bot_avg
     return z_pos
 
 #df_rs = pd.DataFrame(data=pyball.retrosheet.season_game_logs('2023'))
 
-df_sc = correct_call(start_dt = "2023-03-30", end_dt = "2023-04-01")
+df_sc = correct_call(start_dt = "2023-03-30", end_dt = "2023-10-01")
 
-#missed_call_plotting(df_sc)
-
-#df_sc = red_analysis(df_sc)
-
-#print(df_sc)
+missed_call_plotting(df_sc)
 
 #df_sc.to_csv("statcast_data_calls_all_red.csv", index=False, encoding='utf8')
